9fa686ffbd9c5b86c21a636389156da427362274
SEESAndrew <drewlorello@yahoo.com>
2022-04-06 10:17:52 -0400

    Initial commit and project structure

---
+++ app/src/main/java/com/example/taverncrawler/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.taverncrawler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * An {@link AppCompatActivity} class that handles navigation between fragments
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
