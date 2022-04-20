dea4159d684decc175c08f0dcbfdb8ef4a702d53
Tyler Etler <tee5@njit.edu>
2022-04-20 10:41:47 -0400

    Fixed ParseApplication.java

--- app/src/main/java/com/example/taverncrawler/ParseApplication.java
+++ app/src/main/java/com/example/taverncrawler/ParseApplication.java
@@ -2,11 +2,7 @@ package com.example.taverncrawler;
 
 import android.app.Application;
 
-<<<<<<< HEAD
-import com.example.taverncrawler.models.User;
-=======
 import com.example.taverncrawler.models.Review;
->>>>>>> 5e7b118560ddda467c8815d7d75a1ab66959eb05
 import com.parse.Parse;
 import com.parse.ParseObject;
 
@@ -15,12 +11,8 @@ public class ParseApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-<<<<<<< HEAD
-        ParseObject.registerSubclass(User.class);
-=======
         ParseObject.registerSubclass(Review.class);
 
->>>>>>> 5e7b118560ddda467c8815d7d75a1ab66959eb05
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId(getString(R.string.back4app_id))
                 .clientKey(getString(R.string.back4app_ck))
