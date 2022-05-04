2281b08884770fd9d5d98d2d683bbc39889c3819
SEESAndrew <drewlorello@yahoo.com>
2022-05-03 23:43:28 -0400

    ZipCode bug fixes

--- app/src/main/java/com/example/taverncrawler/fragments/ChangeZipcodeFragment.java
+++ app/src/main/java/com/example/taverncrawler/fragments/ChangeZipcodeFragment.java
@@ -1,6 +1,8 @@
 package com.example.taverncrawler.fragments;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -23,54 +25,20 @@ import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ChangeZipcodeFragment#newInstance} factory method to
- * create an instance of this fragment.
  */
 public class ChangeZipcodeFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public ChangeZipcodeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ChangeZipcodeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ChangeZipcodeFragment newInstance(String param1, String param2) {
-        ChangeZipcodeFragment fragment = new ChangeZipcodeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_change_zipcode, container, false);
         Context context = view.getContext();
@@ -79,26 +47,42 @@ public class ChangeZipcodeFragment extends Fragment {
         buttonchange.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                     String change = txtchange.getText().toString();
+                    char[] validString = change.toCharArray();
+                    for(char c : validString) {
+                        if(!Character.isDigit(c)) {
+                            AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
+                            CharSequence[] options = {"Okay"};
+                            builder.setTitle("Invalid Zipcode");
+                            builder.setItems(options, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    if(options[i].equals("Okay")) {
+                                        dialogInterface.dismiss();
+                                    }
+                                }
+                            });
+                            builder.show();
+                            return;
+                        }
+                    }
                     ParseQuery<ParseUser> query = ParseUser.getQuery();
-                    query.whereEqualTo("zipcode", ParseUser.getCurrentUser());
+                    query.whereEqualTo("username", ParseUser.getCurrentUser().getUsername());
                     query.setLimit(1);
                     query.findInBackground(new FindCallback<ParseUser>() {
                         @Override
                         public void done(List<ParseUser> objects, ParseException e) {
                             if(e != null) {
                                 Log.e("Settings", "ParseError: ", e);
-                                Toast.makeText(getContext(), "A problem occurred saving your image", Toast.LENGTH_SHORT).show();
                                 return;
                             }
                             ParseUser user = objects.get(0);
                             user.put("zipcode",change);
                             user.saveInBackground();
+                            txtchange.setText("");
+                            Toast.makeText(requireActivity(), "Successfully saved zipcode!", Toast.LENGTH_SHORT).show();
                         }
                     });
-
-
             }
         });
 
